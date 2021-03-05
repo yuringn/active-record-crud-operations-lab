41be4ac0475fc920070ad961b8023bb46fc739b4
Yuri Nguyen <yuringuyen312@gmail.com>
2021-03-04 19:52:52 -0500

    commit tree 7d31b17bd633f11be5fee9f94722b6dd8c3954dc

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -6,9 +6,9 @@
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = __
+  movie = Movie.new
   movie.title = "This is a title."
-  __
+  movie.save
 end
 
 def can_be_created_with_a_hash_of_attributes
@@ -20,21 +20,26 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = __
+  movie = Movie.new(attributes)
+  movie.save
+  movie
 end
 
-def can_be_created_in_a_block(args = __)
+def can_be_created_in_a_block(args)
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
   
   Movie.create do |m|
-    __
+    m.title = "Home Alone"
+    m.release_date = 1990
+    m.save
+    m
   end
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  Movie.
 end
 
 def can_get_the_last_item_in_the_database
